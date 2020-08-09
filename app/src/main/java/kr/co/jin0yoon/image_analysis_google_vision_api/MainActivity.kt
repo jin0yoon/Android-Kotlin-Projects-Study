7f51c1059dd7103032460e3a941766d6bdc2f183
Yoon Jinyeong <wwwz4805@naver.com>
2020-08-09 20:32:09 +0900

    Update MainActivity.kt
    Create camera function

--- app/src/main/java/kr/co/jin0yoon/image_analysis_google_vision_api/MainActivity.kt
+++ app/src/main/java/kr/co/jin0yoon/image_analysis_google_vision_api/MainActivity.kt
@@ -1,16 +1,23 @@
 package kr.co.jin0yoon.image_analysis_google_vision_api
 
 import android.Manifest
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.util.Log
+import androidx.core.content.FileProvider
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
     //구별하기 위한 식별자
     private val CAMERA_PERMISION_REQUEST = 1000
     private val GALLERY_PERMISION_REQUEST = 1001
+
+    private val FILE_NAME = "picture.jpg"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,14 +51,51 @@ class MainActivity : AppCompatActivity() {
 
     private fun checkCameraPermission(){
         //유틸리티 사용
-        PermissionUtil().requestPermission(
-            this, CAMERA_PERMISION_REQUEST, Manifest.permission.CAMERA
-        )
+        if(PermissionUtil().requestPermission(
+            this, CAMERA_PERMISION_REQUEST, Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE)
+        ){
+            openCamera()
+        }
     }
     private fun checkGalleryPermission(){
         //유틸리티 사용
         PermissionUtil().requestPermission(
-            this, GALLERY_PERMISION_REQUEST, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.CAMERA
+            this, GALLERY_PERMISION_REQUEST, Manifest.permission.READ_EXTERNAL_STORAGE)
+    }
+
+    private fun openCamera(){
+        //사진을 저장할 Uri(경로)
+        val photoUri = FileProvider.getUriForFile(this, applicationContext.packageName + ".provider", createCameraFile())
+
+        //camera를 여는 부분
+        startActivityForResult(
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).apply {
+            putExtra(MediaStore.EXTRA_OUTPUT, photoUri)
+            addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+        }, CAMERA_PERMISION_REQUEST
         )
+    }
+
+    private fun createCameraFile():File{
+        val dir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File(dir, FILE_NAME)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            GALLERY_PERMISION_REQUEST -> {
+
+            }
+            CAMERA_PERMISION_REQUEST -> {
+                if (PermissionUtil().permissionGranted(requestCode, CAMERA_PERMISION_REQUEST, grantResults)){
+                    openCamera()
+                }
+            }
+        }
     }
 }
