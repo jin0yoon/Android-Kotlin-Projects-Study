83483ec231a12d4e6ec0a882f4c220b1ab9a4301
Yoon Jinyeong <wwwz4805@naver.com>
2020-08-10 19:01:25 +0900

    Update Image upload function

--- app/src/main/java/kr/co/jin0yoon/image_analysis_google_vision_api/MainActivity.kt
+++ app/src/main/java/kr/co/jin0yoon/image_analysis_google_vision_api/MainActivity.kt
@@ -1,7 +1,10 @@
 package kr.co.jin0yoon.image_analysis_google_vision_api
 
 import android.Manifest
+import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
@@ -9,6 +12,7 @@ import android.provider.MediaStore
 import android.util.Log
 import androidx.core.content.FileProvider
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.main_analyze_view.*
 import java.io.File
 
 class MainActivity : AppCompatActivity() {
@@ -76,6 +80,31 @@ class MainActivity : AppCompatActivity() {
         )
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when(requestCode){
+            CAMERA_PERMISION_REQUEST -> {
+                //RESULT_OK는 작업이 잘 되었는지 안되었는지 -> 작업이 잘 되었다는 것이 RESULT_OK)
+                if (resultCode != Activity.RESULT_OK) return  //작업이 잘 되지 않았다면 return해서 나가도록
+
+                val photoUri = FileProvider.getUriForFile(this, applicationContext.packageName + ".provider", createCameraFile())
+                uploadImage(photoUri)
+            }
+        }
+    }
+
+    private fun uploadImage(imageUri:Uri){
+        //사진 type은 Bitmap
+        val bitmap : Bitmap = MediaStore.Images.Media.getBitmap(contentResolver, imageUri)   //imageUri의 이미지의 Bitmap을 가져옴
+        //java에서는 getContentResolver로 가져와야 함
+        //kotlin에서는 getter, setter 구분이 없으므로 그냥 contentResolver로 가져오면 됨
+
+        //bitmap을 ImageView에 넣어주면 됨
+        uploaded_image.setImageBitmap(bitmap)
+    }
+
+    //사진 file을 만드는 함수
     private fun createCameraFile():File{
         val dir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
         return File(dir, FILE_NAME)
