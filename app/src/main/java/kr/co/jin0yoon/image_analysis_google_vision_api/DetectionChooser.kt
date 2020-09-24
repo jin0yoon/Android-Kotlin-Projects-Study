9e597553da0d9ad98a98db7159b6d4846bec237f
Yoon Jinyeong <wwwz4805@naver.com>
2020-09-24 20:49:25 +0900

    Create DetectionChooser.kt

---
+++ app/src/main/java/kr/co/jin0yoon/image_analysis_google_vision_api/DetectionChooser.kt
@@ -0,0 +1,22 @@
+package kr.co.jin0yoon.image_analysis_google_vision_api
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+
+class DetectionChooser : DialogFragment(){
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+}
